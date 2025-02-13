e98d695ebd787ea3369f63f07a4e9177a538f0a3
Sylvain78 <sylvain_kerjean@hotmail.com>
2025-02-13 20:32:06 +0100

    Notation IN + SUBSET is prefix

--- ens.mm
+++ ens.mm
@@ -9,6 +9,13 @@ ax-theorie_ensembles_egalitaire $a THEORIE_EGALITAIRE THEORIE_ENSEMBLES $.
 ax-IN $a SIGNE_SPECIFIQUE THEORIE_ENSEMBLES IN $.
 
 ${
+$v T_ U_ $.
+ax-IN_notation.tT $f ASSEMBLAGE T_ $.
+ax-IN_notation.tU $f ASSEMBLAGE U_ $.
+ax-IN_notation $a CM T_ IN U_ =^= IN T_ U_ $.
+$}
+
+${
 $v l_ $.
 ax-theorie_ensembles_sans_lettres.tx $f LETTRE l_ $.
 ax-theorie_ensembles_sans_lettres $a NON_CONSTANTE l_ THEORIE_ENSEMBLES $.
@@ -16,10 +23,10 @@ $}
 $( D'efINition 1. $)
 $c SUBSET $.
 ${
-df-subset $a x SUBSET y =^= ( forall z ) ( ( z IN x ) => ( z IN y ) ) $.
+df-subset $a SUBSET x y =^= ( forall z ) ( ( z IN x ) => ( z IN y ) ) $.
 $}
 
 $( Proposition 1. $)
 ${
-Proposition_1 $p x SUBSET x $= C27 $.
+Proposition_1 $p SUBSET x x $= C27 $.
 $}
